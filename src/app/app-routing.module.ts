d43c701d144c97a4f5a2b01b5cc3484d627c6177
glodzienski <crystopher.glodzienski@gmail.com>
2019-05-13 23:16:14 -0300

    - Criada arquitetura básica do projeto, separação de pacotes. - Criado Componentes Páginas básicos: Login, Register e Home. - Criado Guard de autenticação. - Criado Service de autenticação. - Criado Model de usuário.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {AuthGuard} from '@/_guard/auth.guard';
+import {HomeComponent} from '@/page/home/home.component';
+import {LoginComponent} from '@/page/login/login.component';
+import {RegisterComponent} from '@/page/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: HomeComponent, canActivate: [AuthGuard]},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    // otherwise redirect to home
+    {path: '**', redirectTo: ''}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {HomeComponent} from './page/home/home.component';
+import {LoginComponent} from './page/login/login.component';
+import {RegisterComponent} from './page/register/register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
