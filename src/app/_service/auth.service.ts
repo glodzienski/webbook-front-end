8fe399ee51ffc3f005f0c466bdee1c69979ce82f
glodzienski <crystopher.glodzienski@gmail.com>
2019-05-16 20:06:29 -0300

    - Feito layout do registro

--- src/app/_service/auth.service.ts
+++ src/app/_service/auth.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {AuthLoginDTO} from '@/_dto';
 import {HttpService} from '@/_service/http.service';
+import {AuthTokenDTO} from '@/_dto/AuthTokenDTO';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -18,7 +19,7 @@ export class AuthService {
     }
 
     login(authLoginDTO: AuthLoginDTO) {
-        return this.httpService.$_post<string>('auth/login', authLoginDTO)
+        return this.httpService.$_post<AuthTokenDTO>('auth/login', authLoginDTO)
             .then(({ token }) => {
                 localStorage.setItem('token', JSON.stringify(token));
                 this.currentUserSubject.next(token);
--- src/app/page/register/register.component.ts
+++ src/app/page/register/register.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthLoginDTO} from '@/_dto';
+import {AlertService, AuthService} from '@/_service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.less']
+    selector: 'app-login',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.less']
 })
 export class RegisterComponent implements OnInit {
+    registerForm: FormGroup;
 
-  constructor() { }
+    constructor(private authService: AuthService,
+                private alertService: AlertService,
+                private formBuilder: FormBuilder,
+                private route: ActivatedRoute,
+                private router: Router) {
+        if (this.authService.currentUserValue) {
+            this.router.navigate(['/']);
+        }
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit(): void {
+        this.registerForm = this.formBuilder.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required],
+            cpf: ['', Validators.required],
+            birthdayDate: ['', Validators.required],
+            name: ['', Validators.required],
+            lastName: ['', Validators.required]
+        });
+    }
 
+    get f() {
+        return this.registerForm.controls;
+    }
+
+    public onClickRegister = () => {
+        if (this.registerForm.invalid) {
+            return;
+        }
+        debugger;
+
+        const payload = new AuthLoginDTO(this.registerForm.value.email, this.registerForm.value.password);
+        this.authService.login(payload)
+            .then(_ => this.router.navigate([this.returnUrl]))
+            .catch((error) => this.alertService.error(error));
+    }
 }
