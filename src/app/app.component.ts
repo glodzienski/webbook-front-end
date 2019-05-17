22218e05835e136aa94a9972c24133097f2ae687
LuucasCardoso <lucashgcardoso@outlook.com>
2019-05-16 21:30:45 -0300

    Updating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import {MediaMatcher} from '@angular/cdk/layout';
+import {AuthService} from '@/_service'
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.less']
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent implements OnDestroy, OnInit {
     mobileQuery: MediaQueryList;
+
+    isLogged: Boolean;
 
     menuItems = [
         'Obras',
@@ -17,7 +20,7 @@ export class AppComponent implements OnDestroy {
 
     private mobileQueryListener: () => void;
 
-    constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private authService: AuthService) {
         this.mobileQuery = media.matchMedia('(max-width: 600px)');
         this.mobileQueryListener = () => changeDetectorRef.detectChanges();
         this.mobileQuery.addListener(this.mobileQueryListener);
@@ -27,4 +30,7 @@ export class AppComponent implements OnDestroy {
         this.mobileQuery.removeListener(this.mobileQueryListener);
     }
 
+    ngOnInit(): void {
+        this.isLogged = !!this.authService.currentUserValue;
+    }
 }
